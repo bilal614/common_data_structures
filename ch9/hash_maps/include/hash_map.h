a1ef45b2bcb05d0b8c0395a8524430fa47ae6ca7
bilal614 <bilalbutt.614@gmail.com>
2018-08-12 21:42:15 +0200

    hash maps find operations

--- ch9/hash_maps/include/hash_map.h
+++ ch9/hash_maps/include/hash_map.h
@@ -81,16 +81,81 @@ bool HashMap<K,V,H>::Iterator::operator==(HashMap<K,V,H>::Iterator& p) const
 }
 
 template <typename K, typename V, typename H>
-HashMap<K,V,H>::Iterator& HashMap<K,V,H>::Iterator::operator++()
+typename HashMap<K,V,H>::Iterator& HashMap<K,V,H>::Iterator::operator++()
 {
 	++ent;//next entry in bucket
-	if(endOfBkt(*this))//check if we are at end of this bucket
+	if(endOfBkt(*this))//check if we are at end of this bucket, NOTE: you must dereference the Iterator to provide argument of Iterator& type
 	{
-		
+		++bkt;//move to next bucket 
+		while(bkt != ba->end() && bkt->empty())//find a non-empty bucket in bucket array 
+			++bkt;
+		if(bkt == ba->end()) return *this; //check if we have reached end of bucket array
+		ent = bkt->begin();//set ent to first non-empty Entry
 	}
+	return *this;
 }
 
 
+template <typename K, typename V, typename H>
+typename HashMap<K,V,H>::Iterator HashMap<K,V,H>::end()
+{
+	return HashMap<K,V,H>::Iterator(B, B.end()); 
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K,V,H>::Iterator HashMap<K,V,H>::begin()
+{
+	if(empty()) return end();//check if bucket array is empty
+	BItor bkt = B.begin();//start with beginning bucket
+	while(bkt->empty())++bkt;
+	return HashMap<K,V,H>::Iterator(B, bkt, bkt->begin());	
+}
+
+template <typename K, typename V, typename H>
+HashMap<K,V,H>::HashMap(int capacity):n(0), B(capacity)
+{
+}
+
+template <typename K, typename V, typename H>
+int HashMap<K,V,H>::size() const
+{
+	return n;
+}
+
+template <typename K, typename V, typename H>
+bool HashMap<K,V,H>::empty()
+{
+	return (size() == 0);
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K,V,H>::Iterator HashMap<K,V,H>::finder(const K& k)
+{
+	int i = hash(k) % B.size(); //get hash index i
+	BItor bkt = B.begin() + i; //the ith bucket
+	HashMap<K,V,H>::Iterator p(B, bkt, bkt->begin()); //start of the bucket
+	while(!endOfBkt(p) && (*p).key() == k)
+		nextEntry(p);
+	return p;
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K,V,H>::Iterator HashMap<K,V,H>::find(const K& k)
+{
+	HashMap<K,V,H>::Iterator p = finder(k);
+	if(endOfBkt(p))
+		return end();
+	else
+		return p;
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K,V,H>::Iterator HashMap<K,V,H>::inserter(const Iterator& p, const Entry& e)
+{
+	EItor ins = p.bkt->insert(p.ent, e);//insert before p	
+	n++;
+	return HashMap<K,V,H>::Iterator(B, p.bkt, inst);
+}
 
 
 #endif
